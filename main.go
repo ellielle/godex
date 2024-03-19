b2a3674d1077ad8d53028c027a4266dc9d3efa80
ellielle <40385743+ellielle@users.noreply.github.com>
2024-03-19 09:37:11 -0400

    move repl logic out of main

--- main.go
+++ main.go
@@ -1,39 +1,16 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
 )
 
-const pokeAPIURL = "https://pokeapi.co/api/v2/location-area/"
-
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	prefix := "godex > "
-
 	cliDirections := PokeMap{
 		Next:     nil,
 		Previous: nil,
 		Base:     pokeAPIURL,
 	}
 	cliCommands := getCliCommands(&cliDirections)
-
 	fmt.Println("Welcome to Godex, a CLI Pokedex in Go!")
-
-	// Continue prompting until the user enters "exit"
-	for scanner.Text() != "exit" {
-		fmt.Print(prefix)
-		scanner.Scan()
-		// Check for the entered command in the command list
-		command, ok := cliCommands[scanner.Text()]
-		if !ok {
-			fmt.Printf("%v is not a valid command\n", scanner.Text())
-			continue
-		}
-		command.callback()
-	}
-	if err := scanner.Err(); err != nil {
-		fmt.Fprintln(os.Stderr, "reading stdin: ", err)
-	}
+	startREPL(cliCommands)
 }
